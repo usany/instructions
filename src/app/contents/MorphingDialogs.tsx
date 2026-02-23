f2da58369b6f06f6b338de544e2859e705dc70ac
charlie <ckd_qja@naver.com>
2026-02-23 21:10:05 +0900

    cern

--- src/app/contents/MorphingDialogs.tsx
+++ src/app/contents/MorphingDialogs.tsx
@@ -1,14 +1,16 @@
 import CardView from './CardView';
 
 const MorphingDialogs = () => {
+  const now = Date.now();
   const mockMessage = {
-    id: Date.now().toString().slice(0, 7),
+    id: now.toString().slice(-7),
     action: 1,
     locationOne: '서울 중도',
     locationTwo: '자료열람실',
     startTime: Date.now(),
     finishTime: Date.now(),
   }
+  console.log(mockMessage.id)
   return (
     <CardView message={mockMessage}/>
   )
